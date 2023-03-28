352eceb2016160a4f0cb846dd2b298f4da954422
subratsir <business.ttrc@gmail.com>
2023-03-28 13:57:14 +0530

    data binding

--- app/src/main/java/com/example/retrofit2example/ProductsAdapter.kt
+++ app/src/main/java/com/example/retrofit2example/ProductsAdapter.kt
@@ -4,22 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofit2example.databinding.SingleProductBinding
 import com.example.retrofit2example.retrofit.ProductModel
 
 class ProductsAdapter(val allProducts: ArrayList<ProductModel>): RecyclerView.Adapter<ProductsAdapter.ProductsViewHolder>() {
 
-    inner class ProductsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var productTitle: TextView
 
-        init {
-            productTitle = itemView.findViewById(R.id.tvProductTitle)
-        }
+//    inner class ProductsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//        var productTitle: TextView
+//
+//        init {
+//            productTitle = itemView.findViewById(R.id.tvProductTitle)
+//        }
+//    }
+    class ProductsViewHolder(val binding: SingleProductBinding): RecyclerView.ViewHolder(binding.root){
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsViewHolder {
-        val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.single_product, parent, false)
+//        val v = LayoutInflater.from(parent.context)
+//            .inflate(R.layout.single_product, parent, false)
+        val v = DataBindingUtil.inflate<SingleProductBinding>(LayoutInflater.from(parent.context),R.layout.single_product, parent, false)
         return ProductsViewHolder(v)
     }
 
@@ -28,6 +35,7 @@ class ProductsAdapter(val allProducts: ArrayList<ProductModel>): RecyclerView.Ad
     }
 
     override fun onBindViewHolder(holder: ProductsViewHolder, position: Int) {
-        holder.productTitle.setText( allProducts[position].title)
+        //holder.productTitle.setText( allProducts[position].title)
+        holder.binding?.tvProductTitle?.setText( allProducts[position].title)
     }
 }
